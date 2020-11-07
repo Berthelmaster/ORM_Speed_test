eed9feb035211050dfcb4e71d5af9e57d835f0b8
Berthelmaster <berthel0802@gmail.com>
2020-11-07 11:24:22 +0100

    wqd

---
+++ Database_Check/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Database_Check
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Started...");
+
+            // Add Db
+
+
+        }
+    }
+}
